17d539242a847fef7b5f70db8497ae180ff057ff
SeasonedPorks <yanawarood.yk@gmail.com>
2023-01-13 14:32:47 +0700

    can scrape to csv...chat GPT is something

--- backEnd/main.py
+++ backEnd/main.py
@@ -1,24 +1,16 @@
-import csv
-import string
+import requests
 import pandas as pd
-import requests as requests
-import numpy as np
-from bs4 import BeautifulSoup
 
+response = requests.get('https://api.jikan.moe/v4/anime')
+response_json = response.json()
 
-def wrote_data():
-    html_data = requests.get("https://api.jikan.moe/v4/anime", headers={'accept': 'application/json'})
-    response_json = html_data.json()
-    # orders =[ 'mal_id', 'title', 'type', 'score', 'scored_by', 'status', 'episodes', 'aired_from', 'aired_to', 'source',
-    #      'members', 'favorites', 'duration', 'rating', 'nsfw', 'pending_approval', 'premiered_season', 'premiered_year',
-    #      'broadcast_day', 'broadcast_time', 'genres', 'themes', 'demographics', 'studios', 'producers', 'licensors',
-    #      'synopsis', 'background', 'main_picture', 'url', 'trailer_url', 'title_english', 'title_japanese', 'title_synonyms']
-    #
-    # df = pd.DataFrame.sort_values(by=orders)
-    something = response_json["data"][4]
-    print(something)
+# convert json to dataframe
+df = pd.json_normalize(response_json)
 
+# write dataframe to csv file
+df.to_csv('../backEnd/resource/anime_data.csv', index=False)
 
+df = df.T
 
-if __name__ == '__main__':
-    wrote_data()
+# write dataframe to csv file
+df.to_csv('../backEnd/resource/anime_data.csv', index=False)
--- backEnd/test.py
+++ backEnd/test.py
@@ -1,14 +1,2 @@
-import asyncio
-from jikanpy import AioJikan
 
-async def main():
-    async with AioJikan() as aio_jikan:
-        mushishi = await aio_jikan.anime(457)
-        fma = await aio_jikan.manga(25)
-    # You can also construct AioJikan like below, but make sure to close the object
-    aio_jikan_2 = AioJikan()
-    print(mushishi)
-    print(fma)
-    await aio_jikan_2.close()
 
-asyncio.run(main())
